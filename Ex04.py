3225ab78f85927ffa135deb19e0a1e6b94d24156
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-09-28 17:35:06 -0300

    Update Ex04.py

--- Ex04.py
+++ Ex04.py
@@ -2,10 +2,10 @@ num = 0
 
 while num <= 7:
     if num == 7:
-        num = num + 1
-        while num <= 10:
-            print(num)
-            num = num + 1
+        num = num + 1        
     else:
         print(num)
         num = num + 1 
+while num <= 10:
+    print(num)
+    num = num + 1
